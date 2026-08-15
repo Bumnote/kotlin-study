efa1779406a79a91129dac717ae00c28e5a99f30
Yongbum Kim <dt10002@naver.com>
2026-08-15 23:21:35 +0900

    study: kotlin & SpringBoot 기반 payment 프로젝트 init

---
+++ payment/build.gradle.kts
@@ -0,0 +1,77 @@
+plugins {
+    kotlin("jvm") version "2.3.21"
+    kotlin("plugin.spring") version "2.3.21"
+    id("org.springframework.boot") version "4.1.0"
+    id("io.spring.dependency-management") version "1.1.7"
+    kotlin("plugin.jpa") version "2.3.21"
+}
+
+group = "com.laboratory"
+version = "0.0.1-SNAPSHOT"
+description = "payment"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2025.1.2"
+
+dependencies {
+
+    // Web
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+
+    // H2
+    runtimeOnly("com.h2database:h2")
+    implementation("org.springframework.boot:spring-boot-h2console")
+
+    // Spring Data JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // Validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // Open Feign -> FeignClient
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+
+    // Reflect
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // Jackson
+    implementation("tools.jackson.module:jackson-module-kotlin")
+
+    // Test
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+    }
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
