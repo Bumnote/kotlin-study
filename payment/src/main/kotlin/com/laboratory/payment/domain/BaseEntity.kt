7f9b73020cad88fe4c3f4cc7f7587dee1abcda59
Yongbum Kim <dt10002@naver.com>
2026-08-15 23:46:08 +0900

    feat: BaseEntity 공용 Auditing 설정
    - 모든 Entity에 중복으로 적용되는, id, createdAt, updatedAt 데이터를 BaseEntity 파일로 설정했습니다.
    - Entity 객체의 생성과 수정을 감지할 수 있도록 EntityListeners 어노테이션을 적용했습니다.

---
+++ payment/src/main/kotlin/com/laboratory/payment/domain/BaseEntity.kt
@@ -0,0 +1,22 @@
+package com.laboratory.payment.domain
+
+import jakarta.persistence.*
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null // nullable 타입으로 생성
+
+    @CreatedDate
+    lateinit var createdAt: LocalDateTime
+
+    @LastModifiedDate
+    lateinit var updatedAt: LocalDateTime
+}
