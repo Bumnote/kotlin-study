1681e3df5daa1f58363bd4a14ffcce65112d1392
Yongbum Kim <dt10002@naver.com>
2026-08-16 10:42:54 +0900

    feat: 주문 entity 구현
    - 도메인 요구사항에 맞게 주문 entity를 구현했습니다.
    - enum class는 별도의 파일로 분리하여 통합 관리합니다.

---
+++ payment/src/main/kotlin/com/laboratory/payment/domain/OrderTransaction.kt
@@ -0,0 +1,26 @@
+package com.laboratory.payment.domain
+
+import com.laboratory.payment.TransactionStatus
+import com.laboratory.payment.TransactionType
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.ManyToOne
+import java.time.LocalDateTime
+
+@Entity
+class OrderTransaction(
+    val transactionId: Long,
+    @ManyToOne
+    val order: Order,
+    @Enumerated(EnumType.STRING)
+    val transactionType: TransactionType, // 결제, 취소, 망취소
+    @Enumerated(EnumType.STRING)
+    val transactionStatus: TransactionStatus, // 생성, 성공, 실패
+    val transactionAmount: Long,
+    val merchantTransactionId: Long,
+    var payMethodTransactionId: String? = null,
+    var transactedAt: LocalDateTime? = null,
+    var failureCode: String? = null,
+    var description: String? = null
+) : BaseEntity()
