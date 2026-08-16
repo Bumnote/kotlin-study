698400d09c656041b28818f1b789b4fd3cdd9a80
Yongbum Kim <dt10002@naver.com>
2026-08-16 10:41:18 +0900

    feat: kotlin logging 의존성 추가
    - kotlin에는 롬복을 사용할 수 없어, 별도의 로깅 라이브러리가 필요합니다.
    - kotlin 전용 외부 라이브러리 의존성을 추가해서 로깅이 가능하도록 개선했습니다.

--- payment/build.gradle.kts
+++ payment/build.gradle.kts
@@ -46,6 +46,9 @@ dependencies {
     // Jackson
     implementation("tools.jackson.module:jackson-module-kotlin")
 
+    // Logging
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
+
     // Test
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
